e78490fb9414f477278ed60aaff897d6ba7c625a
Danil <danilbystrov8@gmail.com>
2025-11-10 23:36:34 +0300

    fix cors

--- app.js
+++ app.js
@@ -16,12 +16,18 @@ const port = process.env.PORT || 5000;
 // === Middleware ===
 app.use(cookieParser());
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: [
+//       process.env.FRONTEND_URL,
+//       "http://localhost:5173",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL, 
-      "http://localhost:5173",
-    ],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
