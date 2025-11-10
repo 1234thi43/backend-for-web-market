9ca20732bc72ac69ae747349e89d88b5f94d9904
Danil <danilbystrov8@gmail.com>
2025-11-11 01:09:27 +0300

    ,

--- app.js
+++ app.js
@@ -16,15 +16,6 @@ const port = process.env.PORT || 5000;
 // === Middleware ===
 app.use(cookieParser());
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: [
-//       process.env.FRONTEND_URL,
-//       "http://localhost:5173",
-//     ],
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -3,21 +3,21 @@ const User = require("../models/User");
 
 module.exports = async function (req, res, next) {
   try {
-    const token = req.cookies.token;
+    // const token = req.cookies.token;
 
-    if (!token) {
-      return res.status(401).send({ error: "No token provided" });
-    }
+    // if (!token) {
+    //   return res.status(401).send({ error: "No token provided" });
+    // }
 
-    // Проверяем токен через token.js
-    const tokenData = verify(token);
+    // // Проверяем токен через token.js
+    // const tokenData = verify(token);
 
-    const user = await User.findById(tokenData.id);
-    if (!user) {
-      return res.status(401).send({ error: "Authenticated user not found" });
-    }
+    // const user = await User.findById(tokenData.id);
+    // if (!user) {
+    //   return res.status(401).send({ error: "Authenticated user not found" });
+    // }
 
-    req.user = user;
+    // req.user = user;
     next();
   } catch (e) {
     console.error("Auth error:", e.message);
